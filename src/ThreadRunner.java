f79272a7391cf16cd164bb2ae58bf3f78c1136f6
benstpierre <benstpierre@cb5109-32.labs.sce.carleton.ca>
2018-01-29 18:11:34 -0500

    Made some changes

--- src/ThreadRunner.java
+++ src/ThreadRunner.java
@@ -1,8 +1,27 @@
+import java.util.Scanner;
 
 public class ThreadRunner
 {
 	public static void main(String[] args)
 	{
+		boolean mode = false;
+		Scanner reader = new Scanner(System.in);
+		while(true)
+		{
+			System.out.println("'Quiet' or 'Verbose'?");
+			String bitches = reader.next();
+			if(bitches.equals("Quiet"))
+			{
+				break;
+			}
+			else if(bitches.equals("Verbose"))
+			{
+				mode = true;
+				break;
+			}
+			else {System.out.println("input must be 'Quiet' or 'Verbose' case sensitive");}
+		}
+		System.out.println(mode);
 		Server serverThread = new Server();
 		Client clientThread = new Client();
 		InterHost ErrorSimThread = new InterHost();
