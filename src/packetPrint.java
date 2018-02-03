b699c8f2b4ece114e4ef444147934fb2a9db2654
Omard <Omard@192.168.2.122>
2018-02-03 15:57:50 -0500

    Removed old print packet method

--- src/packetPrint.java
+++ src/packetPrint.java
@@ -2,23 +2,11 @@ import java.io.UnsupportedEncodingException;
 import java.net.DatagramPacket;
 import java.nio.charset.StandardCharsets;
 
+/*@author: Omar Dawoud*/
+
 public class packetPrint
 {
-	//basic method to system.out.print() packet info in both string and byte form
-		public static void printPacket(String info, DatagramPacket packet)
-		{
-			byte[] received = packet.getData();
-			System.out.println(info + " as String: " + new String(received));
-			//above print causes issue where bytes for 0,1, and 2 show as squares but seems better than alternative
-			//System.out.println(info + " as String: " + received);//the alternative
-			System.out.print(info + " as Bytes: ");
-			for(int j = 0; j < received.length; j++)
-			{
-				System.out.format("%02X ", received[j]);
-			}
-			System.out.print("\n");
-		}
-		
+		//basic method to system.out.print() packet info in both string and byte form	
 		public static void Print(String info, DatagramPacket packet)
 		{	
 			boolean fileprint = false;
