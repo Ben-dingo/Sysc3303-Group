16904339aafee08064aba4625ee69c5364a3cded
Noor Ncho <nrandin@gmail.com>
2018-02-16 14:05:16 -0500

    Created class to handle system errors

---
+++ src/ErrorHandeler.java
@@ -0,0 +1,68 @@
+import java.io.File;
+
+public class ErrorHandeler {
+
+	Client c;
+	private int errorcode;
+
+	public ErrorHandeler(Client c) {
+		this.c = c;
+	}
+
+	private void errorExtract(byte[] data) {
+		String error = new String(data);
+		if (error.equals("1")) {
+			System.out.println("Error Code 01: File not found");
+			System.out.println("Please try again.");
+		} else if (error.equals("2")) {
+			System.out.println("Error Code 02: Access Violation");
+			System.out.println("Please try again.");
+		} else if (error.equals("3")) {
+			System.out.println("Error Code 03: Disk full or allocation exceeded");
+			System.out.println("Please try again.");
+		} else if (error.equals("6")) {
+			System.out.println("Error Code 06: File already Exists");
+			System.out.println("Please try again.");
+		}
+		c.ui();
+	}
+
+	/***
+	 * Checks for any possible errors that may be in relation to the files
+	 * 
+	 * @param filename
+	 * @param request
+	 */
+	private boolean errorcheck(String filename, String request) {
+		File file = new File(filename);
+
+		if (request.equals("read")) {
+			// Error Code 1
+			if (!file.exists()) {
+				errorcode = 1;
+				return false;
+			}
+
+			// Error Code 2
+			if (!file.canRead()) {
+				errorcode = 2;
+				return false;
+			}
+
+		} else if (request.equals("write")) {
+			// Error Code 6
+			if (file.exists()) {
+				errorcode = 6;
+				return false;
+			}
+			// Error Code 2
+			if (!file.canWrite()) {
+				return false;
+			}
+
+			// Error Code 3 - no space on disk
+
+		}
+		return true;
+	}
+}
